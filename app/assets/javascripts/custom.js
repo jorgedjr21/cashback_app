209c8f0e383e0a469301cb651e60c96370bf2def
Jorge Junior <jorgedjr21@gmail.com>
2020-05-13 18:47:27 -0300

    show the offers

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,7 @@
 $('document').ready(function(){
   $('.date').mask('00/00/0000 00:00');
 });
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+});
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @offers = Offer.where(enabled: true).order(:ends_at)
   end
 end
